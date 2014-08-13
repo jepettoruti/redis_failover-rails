183406a93c13902496d6eb4d9b5472a5c23498b0
Jose Pettoruti <jose.pettoruti@surfdome.com>
2014-08-13 14:29:52 +0100

    adding hotfix for redis_failover node disconnect

---
+++ lib/redis_failover/node.rb
@@ -0,0 +1,27 @@
+module RedisFailover
+  Node.class_eval do
+    # Safely performs a redis operation within a given timeout window.
+    #
+    # @yield [Redis] the redis client to use for the operation
+    # @raise [NodeUnavailableError] if node is currently unreachable
+    def perform_operation
+      redis = nil
+      Timeout.timeout(MAX_OP_WAIT_TIME) do
+        redis = new_client
+        yield redis
+      end
+    rescue Exception => ex
+      raise NodeUnavailableError, "#{ex.class}: #{ex.message}", ex.backtrace
+    ensure
+      if redis
+        begin
+          puts '!!!!!!!!!! Quitting Redis connection.'
+          redis.quit
+        rescue Exception => ex
+          raise NodeUnavailableError, "#{ex.class}: #{ex.message}", ex.backtrace
+        end
+      end
+    end
+
+  end
+end
--- redis_failover-rails.gemspec
+++ redis_failover-rails.gemspec
@@ -19,7 +19,7 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ['lib']
 
-  s.add_dependency 'redis_failover', '~> 1.0', '>= 1.0.2'
+  s.add_dependency 'redis_failover', '1.0.2'
 
   s.add_development_dependency 'rake',     '~> 10'
   s.add_development_dependency 'bundler',  '~> 1.3'
